7e523de824c184b3d5d578a0f68dcef08aff8252
Aurelia Melati Suci <116251043+aureliare@users.noreply.github.com>
2024-03-06 22:32:19 +0700

    PBO Lingkaran

---
+++ Lingkaran.java
@@ -0,0 +1,18 @@
+package latihan3;
+
+public class Lingkaran {
+	private int Jari2;
+	
+	public Lingkaran (int Jari2) {
+		this.Jari2 = Jari2;
+	}
+	
+	public void setJari2 (int Jari2) { 
+		this.Jari2 = Jari2;
+	}
+	
+	public int getJari2() {
+		return Jari2;
+	}
+	
+}
